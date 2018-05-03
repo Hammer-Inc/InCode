0c2ce6b8275125fd73b9b51198372691846ca6a1
Avrami <avramihammer@gmail.com>
2018-05-04 00:04:34 +1000

    Working on improving base tutorial flow Removed 'Sample Sheets', Added 'Sample Objects' which are converted into cards. Improves the Sample card tutorial for the end user

--- src/Components/InfoCard.js
+++ src/Components/InfoCard.js
@@ -5,7 +5,6 @@ import {Card, CardHeader, CardText} from "material-ui";
 export default class InfoCard extends Component {
     static propTypes = {
         source: PropTypes.object,
-
     };
 
 
@@ -26,14 +25,15 @@ export default class InfoCard extends Component {
             <CardText expandable={true}>
                 <p>
                     In order to calculate a bit, you perform an exclusive or operation on all it's
-                    sub-components. (An exclusive or is just a fancy way of saying if the sum is even, the result is 0, if it's odd, the result is 1)
+                    sub-components. (An exclusive or is just a fancy way of saying if the sum is even, the result is 0,
+                    if it's odd, the result is 1)
                     The sub components are best defined by the 'Take n, skip n' formula, where n is the
                     position
                     of the parity bit in question.
                 </p>
                 <p>
                     If you wanted to for example, calculate the parity bit P2, you can see above it is
-                    located at position 2. You then starting at P2, take two bits, and skip two bits,
+                    located at position 2. You then starting at P2, take two bits (including P2), and skip two bits,
                     The calculation is defined below.
                 </p>
                 <strong>Hint!</strong> For a more visual example, click the highlight button on P2.
@@ -70,7 +70,8 @@ export default class InfoCard extends Component {
                 {
                     isData ? (
                         <div>
-                            The value of this bit is {obj.value}, it is the bit at position {obj.index - 1} from the
+                            <h6>Raw Data bit</h6>
+                            This is a raw data bit with the value of {obj.value}, it is located at position {obj.index - 1} from the
                             right in the raw data
                         </div>
                     ) : null
