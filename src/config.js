03e4e2ddb47b5713e6b7fe01860617d0eb4d50f3
Avrami Hammer <avramihammer@gmail.com>
2018-05-09 13:34:42 +1000

    Changed app title

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-let apiLocation = "http://hamming-sim-backend-dev.ap-southeast-2.elasticbeanstalk.com";
+let apiLocation = "https://hammingsim.herokuapp.com/";
 let endpoint = "/api/v1/codeword/";
 
 export {apiLocation, endpoint};
