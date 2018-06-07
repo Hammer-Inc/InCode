1ab3b011185edc1ef23d264b8e5417e18361bdbd
Avrami <avramihammer@gmail.com>
2018-06-08 07:46:39 +1000

    Added routing

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import NotFound from "./404";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 ReactDOM.render(
-    <App/>,
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={App}/>
+            <Route component={NotFound} status={404}/>
+        </Switch>
+    </BrowserRouter>,
     document.getElementById('root'));
 registerServiceWorker();
